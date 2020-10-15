7b29d17cf83459be57fc426132d63f35bf278ec7
Lip Cowan <pcowan90@gmail.com>
2020-10-15 11:22:02 -0700

    made some minor changes

---
+++ src/services/conversation-api-service.js
@@ -0,0 +1,10 @@
+import config from '../config'
+import TokenService from './token-service'
+
+// to pass through auth header use the getAuthToken
+// to find username we could use the parseJWT 
+const ConversationApiService = {
+
+}
+
+export default ConversationApiService
---
+++ src/services/message-api-service.js
@@ -0,0 +1,8 @@
+import config from '../config'
+import TokenService from './token-service'
+
+const MessageApiService = {
+
+}
+
+export default MessageApiService
