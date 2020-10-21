812240ec2a455b46665da4ffcc62d9373c490c6b
John Bowser <john.bow7720@gmail.com>
2020-10-21 12:54:56 -0600

    refactoring conversation bubble component; small view refactor complete

--- src/components/ConversationBubble/ConversationBubble.js
+++ src/components/ConversationBubble/ConversationBubble.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
+import UserContext from '../../context/UserContext'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Message from '../Message/Message'
 import'./ConversationBubble.css'
 
 export default class ConversationBubble extends Component {
+  static contextType = UserContext 
+  
   constructor(props) {
     super(props)
     this.state = {
@@ -71,11 +74,43 @@ export default class ConversationBubble extends Component {
 
   // renderSecondToLastMessage () {}
 
+  messageStatusMessage = () => {
+    const { messageData } = this.props
+    if(messageData.length === 0) {
+      return 'No messages in this conversation'
+    }
+    const lastMessage = messageData[messageData.length - 1]
+    //if logged in user was the sender of the most recent message
+    if(lastMessage.sender_id === this.context.user.id) {
+      //message has been created but not sent
+      if(lastMessage.sender_status === 'Pending') {
+        return 'Keep working on your current draft'
+        //message has been sent, other user hasn't started new reply yet
+      } else if(lastMessage.sender_status === 'Sent'){
+        return 'Your message is on it\'s way'
+      } else if(lastMessage.sender_status === 'Awaiting Reply') {
+        return 'Your pal has seen your message'
+      }
+      //if logged in user is the receiver of the most recent message
+    } else if(lastMessage.sender_id !== this.context.user.id) {
+      // Pal has seen logged in user's message and started a reply
+      if(lastMessage.sender_status === 'Pending' && lastMessage.is_read) {
+        return 'Your pal is working on their response'
+      } else if(lastMessage.sender_status === 'Sent') {
+        return 'You have a new message(or on the way once we get the timeout)'
+      }
+    }
+  }
+
   renderSmallView = () => {
     return (
-      <div className='ConversationBubble__convo_card'>
-
-      </div>
+      <button className='ConversationBubble__convo_card' onClick={this.expandBubble}>
+        <h2>{this.props.convoData.pal_name}</h2>
+        <FontAwesomeIcon className='ConversationBubble__pal_icon' icon={this.props.convoData.fa_icon} />
+        <p>Conversation Status: {this.messageStatusMessage()}</p>
+        <p>Conversation Start Date: {this.props.convoData.date_created}</p>
+        <p>Total Messages: {this.props.messageData.length}</p>
+      </button>
     )
   }
 
@@ -84,14 +119,17 @@ export default class ConversationBubble extends Component {
   render() {
     const { view, showForm } = this.state
     return (
-      <section className={view}>
-        {view === 'expanded' && <button onClick={this.shrinkBubble}>Close</button>}
-        <button onClick={this.expandBubble}><h2>{this.props.convoData.pal_name}</h2></button>
-        <FontAwesomeIcon icon={this.props.convoData.fa_icon}/>
-        {(showForm === true && view !== 'small') && <Message convoData={this.props.convoData} messageData={this.props.messageData} newMessage={this.state.newMessage}/>}
-        {(view === 'expanded' && showForm === false) && <button onClick={() => this.toggleReplyForm(this.props.convoData)}>Reply</button>}
-        {(view === 'expanded' && showForm === true) && <button onClick={this.closeReplyForm}>Back to message</button>}
-      </section>
+      <>
+        {view === 'small' ? this.renderSmallView(): ''}
+      </>
+  //     <section className={view}>
+  //       {view === 'expanded' && <button onClick={this.shrinkBubble}>Close</button>}
+  //       <button onClick={this.expandBubble}><h2>{this.props.convoData.pal_name}</h2></button>
+  //       <FontAwesomeIcon icon={this.props.convoData.fa_icon}/>
+  //       {(showForm === true && view !== 'small') && <Message convoData={this.props.convoData} messageData={this.props.messageData} newMessage={this.state.newMessage}/>}
+  //       {(view === 'expanded' && showForm === false) && <button onClick={() => this.toggleReplyForm(this.props.convoData)}>Reply</button>}
+  //       {(view === 'expanded' && showForm === true) && <button onClick={this.closeReplyForm}>Back to message</button>}
+  //     </section>
     )
   }
 }
