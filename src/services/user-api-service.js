22544ba881220e274b1d134224171d3f6d0711eb
Lip Cowan <pcowan90@gmail.com>
2020-10-22 15:36:37 -0700

    confirmed patch and update is  operational - may want to continue with setting up profile route to use profile view or profile form upon clicking an edit button

--- src/services/user-api-service.js
+++ src/services/user-api-service.js
@@ -2,41 +2,17 @@ import config from '../config'
 import TokenService from './token-service'
 
 const UserApiService = {
-<<<<<<< HEAD
   patchUser(bio, location){
-=======
-  getUserInfo(user) {
-    return fetch(`${config.API_ENDPOINT}/api/user`, {
-      headers: {
-        authorization: `bearer ${TokenService.getAuthToken()}`
-      }
-    })
-    .then(res => (!res.ok) ? res.json().then(e => Promise.reject(e)) : res.json())
-  },
-
-  patchUser(bio, location, fa_icon) {
-   
-    const updateFields = {
-      bio,
-      location,
-      fa_icon
-    }
-
->>>>>>> 9b09e2351e8b11c40fa702437605e378ff85ba5a
     return fetch(`${config.API_ENDPOINT}/api/user`, {
       method: 'PATCH',
       headers: {
         'content-type': 'application/json',
         'authorization': `bearer ${TokenService.getAuthToken()}`
       },
-<<<<<<< HEAD
       body: JSON.stringify({
         bio: bio,
         location: location
       }),
-=======
-      body:JSON.stringify(updateFields)
->>>>>>> 9b09e2351e8b11c40fa702437605e378ff85ba5a
     })
     .then(res => 
       (!res.ok)
