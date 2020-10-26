6bbe6c5bfd9e51533677c3ea3acda7151142d2ef
Lip Cowan <pcowan90@gmail.com>
2020-10-26 08:38:24 -0700

    prepared for vercel update

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT:'http://localhost:8000',
+  API_ENDPOINT:'https://intense-dawn-33479.herokuapp.com',
   TOKEN_KEY:'zippal-client-auth-token',
   MESSAGE_TIMEOUT_IN_MS: 12 * 60 * 60 * 1000, 
 }
