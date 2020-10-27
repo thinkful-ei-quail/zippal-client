17fbe36506c9274ca22ad44b83d5237d07a0c4cd
Mathew Murray <the.thran@gmail.com>
2020-10-27 15:00:38 -0400

    snapshot tests for 5 components

--- src/setupTests.js
+++ src/setupTests.js
@@ -65,3 +65,8 @@ library.add(
   faBiking,
   faPlaneDeparture
 )
+
+const Enzyme = require('enzyme');
+const EnzymeAdapter = require('enzyme-adapter-react-16');
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
