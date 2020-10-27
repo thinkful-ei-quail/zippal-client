a60d2b64217823b08fe45ffcfaf3dafca573e4a3
John Bowser <john.bow7720@gmail.com>
2020-10-27 10:19:26 -0600

    a working test, huzzah

--- src/components/ConversationBubble/ConversationBubble.test.js
+++ src/components/ConversationBubble/ConversationBubble.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import testHelpers from '../../services/test-helpers'
 import ConversationBubble from './ConversationBubble'
+import UserContext from '../../context/UserContext'
 
 describe('ConversationBubble Component', () => {
   const testConvos = testHelpers.makeTestConvos()
@@ -9,7 +10,11 @@ describe('ConversationBubble Component', () => {
   const testMessages = testHelpers.makeTestMessages()
   it('renders without crashing', () => {
     const div = document.createElement('div')
-    ReactDOM.render(<ConversationBubble convoData={testConvo} messageData={testMessages}/>, div)
+    ReactDOM.render(
+    <UserContext.Provider value={{user: {id: 1}}}>
+      <ConversationBubble convoData={testConvo} messageData={testMessages}/>
+    </UserContext.Provider>
+    , div)
     ReactDOM.unmountComponentAtNode(div)
   })
 })
