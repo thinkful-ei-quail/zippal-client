0bd013c8de5d16390723fc63eb837858f971c2c4
John Bowser <john.bow7720@gmail.com>
2020-10-27 11:44:09 -0600

    finished header smoke test

---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import UserContext from '../../context/UserContext'
+import Header from './Header'
+
+describe('Header Component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<BrowserRouter><Header /></BrowserRouter>, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+})
