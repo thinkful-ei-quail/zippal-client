cb45d817da70180efdcbc779fffd2761a9a3f87d
John Bowser <john.bow7720@gmail.com>
2020-10-27 11:47:43 -0600

    working through smoke tests

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import UserContext from '../../context/UserContext'
+import UserContext from '../../context/UserContext' //Used to process logout
 import Header from './Header'
 
 describe('Header Component', () => {
