89fac56d40131babf5a197730c41fed40e4a12d6
John Bowser <john.bow7720@gmail.com>
2020-10-28 10:42:49 -0600

    working on conversation bubble styling

--- src/components/ConversationBubble/ConversationBubble.js
+++ src/components/ConversationBubble/ConversationBubble.js
@@ -1,14 +1,14 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import UserContext from '../../context/UserContext'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Message from '../Message/Message'
 import ConversationNotification from '../ConversationNotification/CoversationNotification'
-import'./ConversationBubble.css'
+import './ConversationBubble.css'
 import MessageService from '../../services/message-api-service';
 
 export default class ConversationBubble extends Component {
-  static contextType = UserContext 
-  
+  static contextType = UserContext
+
   constructor(props) {
     super(props)
     this.state = {
@@ -22,9 +22,9 @@ export default class ConversationBubble extends Component {
       hideEditAndReplyButton: false
     }
   }
-  
+
   toggleBubble = () => {
-    window.scrollTo(0,0)
+    window.scrollTo(0, 0)
     this.setState({
       expanded: !this.state.expanded
     })
@@ -35,40 +35,40 @@ export default class ConversationBubble extends Component {
   }
 
 
-  
+
   messageStatusMessage = () => {
     const { messageData } = this.props
-    if(!messageData) {
+    if (!messageData) {
       return 'No messages in this conversation'
     }
-    if(messageData.length === 0) {
+    if (messageData.length === 0) {
       return 'No messages in this conversation'
     }
     const lastMessage = messageData[messageData.length - 1]
     //if logged in user was the sender of the most recent message
-    if(lastMessage.sender_id === this.context.user.id) {
+    if (lastMessage.sender_id === this.context.user.id) {
       //message has been created but not sent
-      if(lastMessage.sender_status === 'Pending') {
+      if (lastMessage.sender_status === 'Pending') {
         return 'Keep working on your current draft'
         //message has been sent, other user hasn't started new reply yet
-      } else if(lastMessage.sender_status === 'Sent'){
+      } else if (lastMessage.sender_status === 'Sent') {
         return 'Your message is on it\'s way'
-      } else if(lastMessage.sender_status === 'Awaiting Reply') {
+      } else if (lastMessage.sender_status === 'Awaiting Reply') {
         return 'Your pal has seen your message'
       }
       //if logged in user is the receiver of the most recent message
-    } else if(lastMessage.sender_id !== this.context.user.id) {
+    } else if (lastMessage.sender_id !== this.context.user.id) {
       // Pal has seen logged in user's message and started a reply
-        if(lastMessage.sender_status === 'Pending' && !lastMessage.is_read) {
+      if (lastMessage.sender_status === 'Pending' && !lastMessage.is_read) {
         return 'Your pal is working on their response'
-      } else if(lastMessage.sender_status === 'Sent') {
-          //find out if message is old enough to read
-          if(MessageService.calculateMessageDeliveryTime(lastMessage.date_sent) >= 6) {
-            return 'You have a new message'
-          } else {
-            return 'Your message will be delivered soon'
-          }
-      } else if(lastMessage.sender_status === 'Awaiting Reply') {
+      } else if (lastMessage.sender_status === 'Sent') {
+        //find out if message is old enough to read
+        if (MessageService.calculateMessageDeliveryTime(lastMessage.date_sent) >= 6) {
+          return 'You have a new message'
+        } else {
+          return 'Your message will be delivered soon'
+        }
+      } else if (lastMessage.sender_status === 'Awaiting Reply') {
         return 'Start a reply message'
       }
     }
@@ -79,7 +79,7 @@ export default class ConversationBubble extends Component {
       <button className='ConversationBubble__convo_card small' onClick={this.toggleBubble}>
         {this.props.messageData.length !== 0 ? <ConversationNotification messageData={this.props.messageData} /> : ''}
         <h2>{this.props.convoData.pal_name}</h2>
-        <div className='ConversationBubble__pal_icon'><FontAwesomeIcon  icon={this.props.convoData.fa_icon} /></div>
+        <div className='ConversationBubble__pal_icon'><FontAwesomeIcon icon={this.props.convoData.fa_icon} /></div>
         <p>Conversation Status: {this.messageStatusMessage()}</p>
         <p>Total Messages: {this.props.messageData.length}</p>
       </button>
@@ -91,27 +91,27 @@ export default class ConversationBubble extends Component {
   renderMessages = () => {
     const { messageData } = this.props
     const { user } = this.context
-    
-    if(messageData.length === 0) {
+
+    if (messageData.length === 0) {
       return <span>No messages to display yet</span>
     }
 
     const messageContainers = messageData.sort((a, b) => a.id - b.id).map((message, i) => {
-     if((message.receiver_id === user.id && message.date_sent && MessageService.calculateMessageDeliveryTime(message.date_sent) >= 6) || (message.sender_id === user.id)) {
-      return (
-        <button 
-          onClick={() => this.selectMessageHandler(i)} 
-          key={message.id} 
-          className='ConersationBubble__message_select'
-          disabled={this.state.selectedMessage}
-        >
-          <p>{message.sender_id === this.context.user.id ? 'Outgoing': 'Incoming'}</p>
-          {message.date_sent ? <p>Date sent: {this.formatDate(message.date_sent)}</p> : ''}
-          <p>Content: {message.content.substring(0, 30)}...</p>
-        </button>
-      )
-     }
-     return null
+      if ((message.receiver_id === user.id && message.date_sent && MessageService.calculateMessageDeliveryTime(message.date_sent) >= 6) || (message.sender_id === user.id)) {
+        return (
+          <button
+            onClick={() => this.selectMessageHandler(i)}
+            key={message.id}
+            className='ConersationBubble__message_select'
+            disabled={this.state.selectedMessage}
+          >
+            <p>{message.sender_id === this.context.user.id ? 'Outgoing' : 'Incoming'}</p>
+            {message.date_sent ? <p>Date sent: {this.formatDate(message.date_sent)}</p> : ''}
+            <p>Content: {message.content.substring(0, 30)}...</p>
+          </button>
+        )
+      }
+      return null
     })
 
     return messageContainers
@@ -124,8 +124,8 @@ export default class ConversationBubble extends Component {
       selectedMessage: selected,
     })
     if (!selected.is_read && selected.receiver_id === this.context.user.id) {
-        MessageService.readMessage(selected)
-      }
+      MessageService.readMessage(selected)
+    }
   }
 
   //Nullifies various pieces of state once a message has been acted upon so that other actions can be taken
@@ -156,14 +156,14 @@ export default class ConversationBubble extends Component {
 
   // render the confirm delete conversation panel
   renderConfirmEndConvoPanel = () => {
-    const {handleEndConvo} = this.props
+    const { handleEndConvo } = this.props
     let convo = (this.props.convoData)
 
     return (
       <>
-      <p>Are you sure you want to delete this conversation? You will lose all of your letters and this cannot be undone.</p>
-      <button className="ConversationBubble__end_convo_confirm_btn" onClick={() => handleEndConvo(convo)}>Confirm</button>
-      <button className="ConversationBubble__end_convo_confirm_btn" onClick={this.cancelEndConvo}>Cancel</button>
+        <p>Are you sure you want to delete this conversation? You will lose all of your letters and this cannot be undone.</p>
+        <button className="ConversationBubble__end_convo_confirm_btn form_button" onClick={() => handleEndConvo(convo)}>Confirm</button>
+        <button className="ConversationBubble__end_convo_confirm_btn form_button" onClick={this.cancelEndConvo}>Cancel</button>
       </>
     )
   }
@@ -194,78 +194,79 @@ export default class ConversationBubble extends Component {
 
   //Render buttons for appropriate actions based on conversation/message status
   renderActionButton = () => {
-      const { convoData, messageData } = this.props
-      const { selectedMessage } = this.state
-      const { user } = this.context
-      if(selectedMessage.sender_id === user.id && selectedMessage.date_sent === null) {
-        return (
-          <button 
-            type='button' 
-            onClick={this.startEditing}
-          >
-            Edit
-          </button>
-        )
-      }
-      if((convoData.user_1 === user.id && convoData.user_1_turn === true && selectedMessage.id === messageData[messageData.length - 1].id)
-      || (convoData.user_2 === user.id && convoData.user_2_turn === true && selectedMessage.id === messageData[messageData.length - 1].id)) {
-        return (
-          <button 
-            type='button'
-            onClick={this.startReply}
-          >
-            Reply
-          </button>
-        )
-      }
+    const { convoData, messageData } = this.props
+    const { selectedMessage } = this.state
+    const { user } = this.context
+    if (selectedMessage.sender_id === user.id && selectedMessage.date_sent === null) {
+      return (
+        <button
+          className='ConversationBubble__action_btn form_button'
+          type='button'
+          onClick={this.startEditing}
+        >
+          Edit
+        </button>
+      )
     }
+    if ((convoData.user_1 === user.id && convoData.user_1_turn === true && selectedMessage.id === messageData[messageData.length - 1].id)
+      || (convoData.user_2 === user.id && convoData.user_2_turn === true && selectedMessage.id === messageData[messageData.length - 1].id)) {
+      return (
+        <button
+          className='ConversationBubble__action_btn form_button'
+          type='button'
+          onClick={this.startReply}
+        >
+          Reply
+        </button>
+      )
+    }
+  }
 
   // conditionally render reply(create new message) button or continue draft(open last message in text area to continue writing)
   renderExpandedView = () => {
     const { selectedMessage, confirmEndConvoPanel, hideMessage, reply, edit, contentOfPrevious, hideEditAndReplyButton } = this.state
     return (
-    <div className='ConversationBubble__convo_card expanded'>
-      
-      {/*Navigation buttons for ConversationBubble */}
-      <div className='ConversationBubble__nav'>
-        <button onClick={this.toggleBubble}>
-          <FontAwesomeIcon className='ConversationBubble__window_close' icon="window-close" />
-        </button>
-        {selectedMessage ? <button onClick={this.clearSelectedMessage}>Go back</button>: ''}
-        <button className="ConversationBubble__end_convo_btn" onClick={this.confirmEndConvo}>
-          End Conversation
-        </button>
-      </div>
-      {/* Message buttons */}
-      <div className='conversationBubble__message_wrapper'>
-      <div className='ConversationBubble__messages_container'>{this.renderMessages()}</div> 
-      {/* Display currently selected message and action buttons */}
-      <div className={`ConversationBubble__content ${selectedMessage  ? " " : "transparent"}`}>
-        {selectedMessage ? <button type='button' onClick={() => this.setState({hideMessage: !this.state.hideMessage})}>{hideMessage ? 'Show message' : 'Hide message'}</button> : '' }
-        {selectedMessage && !hideMessage
-          ? (
-            <>
-            <p>{reply ? contentOfPrevious : selectedMessage.content}</p>
-            {hideEditAndReplyButton ? '' : this.renderActionButton()}
-            </>
-            )
-          : ''}
-      <div className='ConversationBubble__form_container'>
-        { (reply || edit) 
-          ? <Message 
-            convoData={this.props.convoData} 
-            message={this.state.selectedMessage} 
-            setNewMessage={this.props.setNewMessage} 
-            clearSelectedMessage={this.clearSelectedMessage}
-            updateConvoTurns={this.props.updateConvoTurns}
-          />
-          : ''}
-          </div>
-      </div>
-    </div>
-      {confirmEndConvoPanel ? this.renderConfirmEndConvoPanel() : ''}
+      <div className='ConversationBubble__convo_card expanded'>
 
-    </div>
+        {/*Navigation buttons for ConversationBubble */}
+        <div className='ConversationBubble__nav'>
+          <button onClick={this.toggleBubble}>
+            <FontAwesomeIcon className='ConversationBubble__window_close' icon="window-close" />
+          </button>
+          {selectedMessage ? <button onClick={this.clearSelectedMessage}>Go back</button> : ''}
+          <button className="ConversationBubble__end_convo_btn" onClick={this.confirmEndConvo}>
+            End Conversation
+        </button>
+        </div>
+        {/* Message buttons */}
+        <div className='conversationBubble__message_wrapper'>
+          <div className='ConversationBubble__messages_container'>{this.renderMessages()}</div>
+          {/* Display currently selected message and action buttons */}
+          <div className={`ConversationBubble__content ${selectedMessage ? " " : "transparent"}`}>
+            {confirmEndConvoPanel ? this.renderConfirmEndConvoPanel() : ''}
+            {selectedMessage ? <button type='button' onClick={() => this.setState({ hideMessage: !this.state.hideMessage })}>{hideMessage ? 'Show message' : 'Hide message'}</button> : ''}
+            {selectedMessage && !hideMessage
+              ? (
+                <>
+                  <p className='ConversationBubble__message_text'>{reply ? contentOfPrevious : selectedMessage.content}</p><hr/>
+                  {hideEditAndReplyButton ? '' : this.renderActionButton()}
+                </>
+              )
+              : ''}
+            <div className='ConversationBubble__form_container'>
+              {(reply || edit)
+                ? <Message
+                  convoData={this.props.convoData}
+                  message={this.state.selectedMessage}
+                  setNewMessage={this.props.setNewMessage}
+                  clearSelectedMessage={this.clearSelectedMessage}
+                  updateConvoTurns={this.props.updateConvoTurns}
+                />
+                : ''}
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 
