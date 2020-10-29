5ef9591d0b5ea9a1e925ba81a0845b736651feb8
Mathew Murray <the.thran@gmail.com>
2020-10-29 14:51:54 -0400

    Update AboutRoute.js

---
+++ src/routes/AboutRoute/AboutRoute.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+export default class AboutRoute extends Component {
+  render(){
+    return (
+      <>
+        <h2>About Zip pal</h2>
+        <div className='about_container'>
+          <h2>Where did it all start?</h2>
+          <p>Zip Pal started as an idea to connect people during the hight of Covid 19. Many countries have isolation orders
+            in place, restricting what people can do outside of their own homes. These restrictions are meant to slow the
+            spread of Covid 19, but unfortunately also have several adverse affects to mental health.
+          </p>
+
+        </div>
+      </>
+    )
+  }
+}
